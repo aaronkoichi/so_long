05303a9ffe249e1d123cddc11275684ed3103945
aaronkoichi <aaronlee4399@gmail.com>
2025-04-05 20:55:41 +0800

    resize numbers once moew

--- src/bonus/so_long_init_numbers.c
+++ src/bonus/so_long_init_numbers.c
@@ -6,7 +6,7 @@
 /*   By: zlee <zlee@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 18:36:35 by zlee              #+#    #+#             */
-/*   Updated: 2025/04/05 19:15:20 by zlee             ###   ########.fr       */
+/*   Updated: 2025/04/05 20:30:53 by zlee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ void init_numbers(t_data *mlx)
 {
 	t_pos	p;
 
-	p.x = 32;
-	p.y = 64;
+	p.x = 16;
+	p.y = 32;
 	mlx->numbers[0] = create_sprite("./textures/numbers/digit_0.xpm", 
 		mlx->mlx_ptr, p);
 	mlx->numbers[1] = create_sprite("./textures/numbers/digit_1.xpm",
@@ -54,7 +54,7 @@ void	process_number(t_data *mlx)
 	{
 		xpm_image_transparency(mlx, &mlx->numbers[text[i] - '0'],
 				&mlx->d_img, pos);
-		pos.x += MUL;
+		pos.x += 17;
 	}
 	free(text);
 }
